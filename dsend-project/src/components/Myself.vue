dd5a97bce54d7812eb10b1bba6c9a69766afa36e
SnowGoose0 <jason2580134@outlook.com>
2023-08-13 21:39:48 +0800

    marker addition

---
+++ dsend-project/src/components/Myself.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  props: ['selfName'],
+  setup() {
+
+  },
+}
+</script>
+
+<template>
+  <div class="self-identifier">
+    <h6 class="identify-message">I am known as</h6>
+    <h2 class="self">{{ selfName }}</h2>
+  </div>
+</template>
+
+<style lang="scss">
+  .self-identifier {
+    text-align: center;
+    background-color: rgb(15, 15, 15);
+    margin-bottom: 1em;
+    padding: 1.5em;
+
+    .self-identifier {
+      margin-bottom: 0.5em;
+    }
+
+    .self {
+      color: white;
+    }
+  }
+</style>
